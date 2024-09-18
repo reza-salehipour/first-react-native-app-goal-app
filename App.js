4f327cdc2f667c48202f8c6f4885b5e0277bf868
Reza Sal <rez.salehipour@gmail.com>
2024-09-18 08:38:38 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text>Another one!!</Text>
+      </View>
+      <Text>Hello World</Text>
+      <Button title="tap me!" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
